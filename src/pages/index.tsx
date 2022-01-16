77a4ee5b64b1d958f4926d80656122ed28dc8b28
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-01-16 16:14:29 +0000

    cleanup :broom:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,9 @@ import type {NextPage} from 'next';
 import Head from 'next/head';
 import {useRouter} from "next/router";
 
-const Home: NextPage = () => {
+const Index: NextPage = () => {
 
     const router = useRouter();
-
-    const handleLink = (url: string) => {
-        router.push(url);
-    }
 
     return (
         <div className={styles.container}>
@@ -26,12 +22,12 @@ const Home: NextPage = () => {
                 {/*<h1 className={styles.title}>Simply Jetpacks</h1>*/}
 
                 <div className={styles.grid}>
-                    <div className={styles.card} onClick={() => {handleLink("/simplyjetpacks2")}}>
+                    <div className={styles.card} onClick={() => {router.push("/simplyjetpacks2")}}>
                         <h2>Simply Jetpacks 2</h2>
                         <p>Find out more!</p>
                     </div>
 
-                    <div className={styles.card} onClick={() => {handleLink("/fabric")}}>
+                    <div className={styles.card} onClick={() => {router.push("/fabric")}}>
                         <h2>Fabric</h2>
                         <p>Find out more!</p>
                     </div>
@@ -55,4 +51,4 @@ const Home: NextPage = () => {
     )
 }
 
-export default Home;
+export default Index;
