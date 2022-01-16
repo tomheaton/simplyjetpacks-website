fa0e15dabdd611cb2cd01ddbfdf12a9035d8595c
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-01-16 16:13:33 +0000

    Add more pages

---
+++ src/pages/terraria.tsx
@@ -0,0 +1,21 @@
+import {NextPage} from "next";
+import {useRouter} from "next/router";
+
+const Fabric: NextPage = () => {
+
+    const router = useRouter();
+
+    const handleReturn = () => {
+        router.push("/");
+    }
+
+    return (
+        <div className={"container"}>
+            <h1>Simply Jetpacks 2 (Terraria)</h1>
+            <p>Oops, page is under construction... 🏗️</p>
+            <button onClick={handleReturn}>Return Home</button>
+        </div>
+    );
+}
+
+export default Fabric;
