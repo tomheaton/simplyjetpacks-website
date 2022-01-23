2cf8286dd2a1613e70574c8e21f4c46dcaa82521
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-01-23 18:49:31 +0000

    Update forge and fabric pages.

--- src/pages/fabric.tsx
+++ src/pages/fabric.tsx
@@ -1,6 +1,7 @@
 import {NextPage} from "next";
 import {useRouter} from "next/router";
 import Head from "next/head";
+import styles from "../styles/Main.module.css";
 
 const Fabric: NextPage = () => {
 
@@ -13,11 +14,27 @@ const Fabric: NextPage = () => {
                 <meta name="description" content="Simply Jetpacks - Website"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <h1>Simply Jetpacks 2 (Fabric)</h1>
-            <p>Oops, this page is under construction... 🏗️</p>
-            <button className={"btn"} onClick={() => {router.push("/")}}>
-                Return Home
-            </button>
+            <div className={styles.main}>
+                <h1>Simply Jetpacks 2 (Forge)</h1>
+                <div className={styles.badgeContainer}>
+                    <img src={"https://cf.way2muchnoise.eu/simply-jetpacks-2-fabric.svg"}
+                         alt={"CF SJ2 fabric downloads"}/>
+                    <img src={"https://cf.way2muchnoise.eu/versions/simply-jetpacks-2-fabric.svg"}
+                         alt={"CF SJ2 fabric version"}/>
+                </div>
+                <p>
+                    Downloads for this mod can be found
+                    {" "}
+                    <a href={"https://www.curseforge.com/minecraft/mc-mods/simply-jetpacks-2-fabric"}
+                       className={styles.here}>
+                        here
+                    </a>
+                </p>
+                <br/>
+                <button className={"btn"} onClick={() => {router.push("/")}}>
+                    Return Home
+                </button>
+            </div>
         </div>
     );
 }
