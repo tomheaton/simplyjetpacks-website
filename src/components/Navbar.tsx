e4857e0b118bdb9cb203e6dbfc3c0a02a6a83a22
Tom Heaton <thomas.m.heaton@gmail.com>
2022-05-30 00:41:50 +0100

    todo for navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// TODO: create navbar with logo in center and mod page links on the right?
+const Navbar: React.FC = () => {
+
+    return (
+        <nav>
+            navbar
+        </nav>
+    );
+}
+
+export default Navbar;
