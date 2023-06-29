35fb5ecbc16f6b1fef5cb217b1f8349486735332
Tom Heaton <thomas.m.heaton@gmail.com>
2023-06-29 16:08:07 +0100

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,5 +5,17 @@ export default function Sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://simplyjetpacks.com",
     },
+    {
+      url: "https://simplyjetpacks.com/forge",
+    },
+    {
+      url: "https://simplyjetpacks.com/fabric",
+    },
+    {
+      url: "https://simplyjetpacks.com/bedrock",
+    },
+    {
+      url: "https://simplyjetpacks.com/terraria",
+    },
   ];
 }
